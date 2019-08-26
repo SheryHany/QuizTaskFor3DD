d98b207445d86c1024954e2b4dbe1ff9fd40cd6c
SheryHany <sheryhany16@gmail.com>
2019-08-26 16:16:17 +0200

    ha

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require("express");
-const app = express();
+//Install express server
+const express = require('express');
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/task-d4'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/task-d4/index.html'));
 });
 
-console.log("Console listening");
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
